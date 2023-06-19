864038e3dfcbb9b3771eb78f9c4c960ad2931ea5
anisazh01 <135994001+anisazh01@users.noreply.github.com>
2023-06-18 22:15:48 -0500

    Add files via upload

---
+++ Module 7 lab activity/Module 5 lab activity/w5p1.py
@@ -0,0 +1,6 @@
+# Alina Zholdubaeva
+# 06/18/2023
+# The program uses a for loop to print “Hello world” 100 times
+
+for _ in range(100):
+    print("Hello world")
---
+++ Module 7 lab activity/Module 5 lab activity/w5p2.py
@@ -0,0 +1,11 @@
+# Alina Zholdubaeva
+# 06/18/2023
+# The program takes the following list of numbers and use a for loop to print each number and its
+# square to the screen, each value on a new line:
+# num_list = [12, 10, 32, 3, 66, 17, 42, 99, 20]
+
+num_list = [12, 10, 32, 3, 66, 17, 42, 99, 20]
+
+for num in num_list:
+    square = num ** 2
+    print(f"Number: {num}, Square: {square}")
